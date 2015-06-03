c700c32e744517f21373c9c604982b8969854271
Miceal Gallagher <hawk@localhost.localdomain>
2015-06-02 23:16:20 -0400

    Getting some scope issues while trying to get the settings and apply them to their respective controls. Hopefully this resolves things.

--- js/app.js
+++ js/app.js
@@ -15,18 +15,21 @@ angular.module('ChillaxApp', [])
         settings["breakInterval"] = "";
         settings["sound"] = "";
         
+        chillax.reminderInterval = settings["reminderInterval"];
+
         chillax.enabled = true;
         
         chrome.storage.sync.get(settings, function(obj) {
             
-            console.log("Settings loaded: " + obj["enabled"]);
-            
-            this.chillax.reminderInterval = obj["reminderInterval"];
-            chillax.breakInterval = settings["breakInterval"];
-            chillax.sound = settings["sound"];
+            console.log("Settings loaded: " + obj["reminderInterval"]);
+            settings["reminderInterval"] = obj["reminderInterval"];
+
+            chillax.reminderInterval = settings["reminderInterval"];
         });
-        
+
     };
+
+
     
 	chillax.playSound = function () {
 		chillax.nextNotification="Is enabled: " + chillax.enabled;
