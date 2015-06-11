8ad139723231f9c38dfdb8a4e8390cb0865b7c0d
user.name <me@mehaul.me>
2015-06-11 10:04:52 -0400

    Make Chillax a frameless app so that we can hide the maximize button

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
-    "bounds": {
+      "frame" : "none",
+      "bounds": {
       "width": 400,
       "height": 310
-    }
+    },
   });
+    chrome.app.window.onClosed.addListener(function() {
+        console.log('Closing...boss');
+    });
+
 });
--- js/app.js
+++ js/app.js
@@ -2,6 +2,59 @@ angular.module('ChillaxApp', [])
     .controller('ChillaxController', ['$scope', 'setting', 'sound', 'notification', 
     function($scope, setting, sound, notification) {
         
+        
+        
+        
+        
+        // Make sure we read the initial state as well, since the app might startup as maximized.
+    $scope.isMaximized = chrome.app.window.current().isMaximized();
+
+    $scope.handleWindowEvents = function () {
+        // Happens when user uses the window bar or shortcuts to maximize.
+        $scope.isMaximized = chrome.app.window.current().isMaximized();
+
+        // This happens from an event and therefore we need to run $apply to make the UI update.
+        $scope.$apply();
+    };
+    
+    chrome.app.window.current().onMaximized.addListener($scope.handleWindowEvents);
+    chrome.app.window.current().onMinimized.addListener($scope.handleWindowEvents);
+    chrome.app.window.current().onRestored.addListener($scope.handleWindowEvents);
+
+    $scope.minimize = function ()
+    {
+        chrome.app.window.current().minimize();
+    }
+
+    $scope.maximize = function () {
+        chrome.app.window.current().maximize();
+    }
+
+    $scope.restore = function () {
+        chrome.app.window.current().restore();
+    }
+
+    $scope.close = function () {
+        window.close();
+    }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         $scope.setting = setting;
         $scope.sound = sound;
         $scope.notification = notification;
